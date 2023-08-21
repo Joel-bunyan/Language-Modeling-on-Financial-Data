8d88562dd7768d26b2853c79bc91ca7227addd9b
Joel Bunyan <80108818+Joel-bunyan@users.noreply.github.com>
2023-08-21 21:57:42 +0530

    Removing the code and putting it on a different file and accessing it through a function. Todo: Get the Model pipeline from Langchain

--- main.py
+++ main.py
@@ -1,31 +1,5 @@
 import psycopg2
 from psycopg2 import sql
+from Dataset_Loader import load_data
 
-try:
-    connection = psycopg2.connect(
-        user="postgres",
-        password="joelbunyan",
-        host="localhost",
-        port="5432"
-    )
-    
-    connection.autocommit = True
-
-    cursor = connection.cursor()
-
-except psycopg2.Error as e:
-    print("Error while connecting to PostgreSQL:", e)
-    exit()
-
-try:
-    database_name = "Finance data"
-    create_db_query = sql.SQL("CREATE DATABASE {}").format(sql.Identifier(database_name))
-    cursor.execute(create_db_query)
-    print("Database created successfully!")
-
-except psycopg2.Error as e:
-    print("Error while creating database:", e)
-
-finally:
-    cursor.close()
-    connection.close()
+load_data()
